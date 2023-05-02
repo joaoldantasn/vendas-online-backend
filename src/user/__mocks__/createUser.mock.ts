8b6ba882eade239b887c85fc884f13b3af716712
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-05-02 14:13:26 -0300

    teste

---
+++ src/user/__mocks__/createUser.mock.ts
@@ -0,0 +1,9 @@
+import { createUserDto } from '../dtos/createUser.dto';
+
+export const createUserMock: createUserDto = {
+  cpf: '3214215151',
+  email: 'emailMockTest@email.com',
+  name: 'qudlsjakf',
+  password: 'password',
+  phone: '325632634',
+};
